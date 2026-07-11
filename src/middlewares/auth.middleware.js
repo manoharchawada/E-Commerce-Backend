b6e0ace1e0b2afe0b27f86f314ed3447949d3c68
manohar <manohar.chawada@idealittechno.com>
2026-07-11 12:27:37 +0530

    Complete User Authentication & Profile API Integration

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,40 @@
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiErrors.js";
+import { User } from "../models/user.model.js";
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+  try {
+    // fetch the token from cookie or head
+    // check token if not exist then throw error
+    // decode the token with jwt verify method
+    // fetch user from db by id
+    // after fetch add the user in req.user = user then next()
+    const token =
+      req.cookies?.accessToken ||
+      req.header("Authorization")?.replace("Bearer", "");
+    console.log(token, "000000000");
+
+    if (!token) {
+      return res.status(401).json(new ApiError(401, "Unauthorized request"));
+    }
+    const decodedToken = await jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+    if (!user) {
+      return res.status(401).json(new ApiError(401, "User not found"));
+    }
+    req.user = user;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json(new ApiError(401, error?.message || "Invalid access token"));
+  }
+});
+
+export { verifyJWT };
